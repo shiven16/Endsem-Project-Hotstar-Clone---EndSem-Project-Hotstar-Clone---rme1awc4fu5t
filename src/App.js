4cf74670afab02a2d0790593a87d8cd146b53e06
Shiven Ahuja <shivenahuja@Shivens-MacBook-Pro.local>
2024-05-28 16:04:37 +0530

    f

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import apiContextProvider from "./Components/Context.js";
+import SignUp from "./Components/SignUp";
+import Login from "./Components/Login.js";
 
-
+import Homepage from "./Components/Homepage";
+import Error from "./Components/Error";
+import AllShows from "./Components/AllShows.js";
+import Navbar from "./Components/Navbar.js";
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  const router = createBrowserRouter([
+
+    {path:"/",element:<Homepage/>,children:[
+      {index:true, element:<AllShows/>}
+    ]},
+    {path:"/signup",element:<SignUp/>},
+    {path:"/login",element:<Login/>},
+    {path:"/error",element:<Error/>},
+    {path:"*",element:<SignUp/>},
+    ]);
+  return(
+    <apiContextProvider>
+      <RouterProvider router={router} />
+    </apiContextProvider>
+  ) 
 }
 
 export default App;
